d2409bc68bc4cace4e8b76ad0d4b9414f5bccdc3
0xbm <__malinkA+842!>
2022-05-15 15:59:55 +0200

    initial commit

--- budget.py
+++ budget.py
@@ -1,1 +1,14 @@
-import openpyxl
+from openpyxl import Workbook
+
+class CreateFile:
+    wb = Workbook()
+    ws = wb['Sheet']
+    ws['A1'] = 'POJAZDY'
+    ws['B1'] = 'ILOSC PALIWA'
+    ws['C1'] = 'WAGA PALIWA'
+    ws['D1'] = 'CENA'
+    wb.save("budget.xlsx")
+
+
+CreateFile()
+
